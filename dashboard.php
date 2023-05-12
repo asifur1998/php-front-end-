4e638a6bbe8a284f075f52616caa09f3492c1c9e
Asifur rahman <112628214+asifur1998@users.noreply.github.com>
2023-05-12 21:36:07 +0600

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,191 @@
+<html>
+<head>
+<title> dashboard </title>
+<link rel="stylesheet"href="style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">NGICON Task_1</a>
+	    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+		
+          <a class="nav-link dropdown-toggle" href="http://localhost/task_1/dashboard.php?" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dashboard
+          </a>
+
+	
+
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+		   <li><a class="dropdown-item" href="http://localhost/task_1/dashboard.php">Dashboard</a></li>
+            
+		  <li><a class="dropdown-item" href="index.php">Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+	  
+     
+    </div>
+  </div>
+</nav>
+
+
+
+
+<div class="full_wrapper"> 
+<img src="images/task-1.png" alt="image not found" width="100%;" height="300px;">
+
+<div class="navbar"> 
+<div class="min_wrapper"> 
+
+
+
+<div class="form">
+<form action=""method ="post">
+ <div class="col-lg-12"> 
+ <input type="text" class="form-control"placeholder="job title, keywords">
+ </div>
+ </form>
+ </div>
+ 
+ 
+  <div class="dropdown">
+ <div class="col-lg-12"> 
+ 
+ <select name="cars" id="cars"class="form-control">
+    <option value="volvo">Select</option>
+    <option value="dubai">Dubai</option>
+    <option value="abu dhabi">Abu Dhabi</option>
+    <option value="sharjah">Sharjah</option>
+    <option value="ajman">Ajman</option>
+    <option value="fujairah">Fujairah</option>
+    <option value="ras al khaimah">Ras Al Khaimah</option>
+  </select>
+ 
+ </div>   
+ </div>
+ 
+ <div class="search_icon">
+ 
+ <a href="https://www.omb.ae/jobsearch"><button type="button" class="btn btn-primary"><i class="fa fa-search"></i></button></a>
+ </div>
+ 
+</div>
+</div>
+<div class="body">
+<div class="min_wrapper"> 
+<table class="table">
+ 
+  <tbody>
+  
+	
+	  <tr>
+     
+       <td><img src="images/task1a.png" alt="image not found" width="100px;" height="100px;"> </td>
+      <td><h5><b> <a href="https://www.omb.ae/jobs/Sr.-Developer---Fraud-Management"> Sr.Developer-Fraud Management </a>  </h5> </b> </td>
+     
+    </tr>
+	
+    <tr>
+     
+       <td><img src="images/task1a.png" alt="image not found" width="100px;" height="100px;"> </td>
+      <td><h5><b> <a href="https://www.omb.ae/jobs/Sr.-Developer---Fraud-Management"> Manager - PDC Mobile & M2M Services PD </a>  </h5> </b> </td>
+     
+    </tr>
+	
+	 <tr>
+     
+       <td><img src="images/task1a.png" alt="image not found" width="100px;" height="100px;"> </td>
+      <td><h5> <b><a href="https://www.omb.ae/jobs/Sr.-Developer---Fraud-Management">Administrator - workplace support </a></h5></b></td>
+     
+    </tr>
+	
+	<tr>
+     
+       <td><img src="images/task1a.png" alt="image not found" width="100px;" height="100px;"> </td>
+      <td><h5><b>  <a href="https://www.omb.ae/jobs/Sr.-Developer---Fraud-Management">Cloud Orchestration </a> </h5></b></td>
+     
+    </tr>
+	
+	<tr>
+     
+       <td><img src="images/task1a.png" alt="image not found" width="100px;" height="100px;"> </td>
+      <td><h5> <b> <a href="https://www.omb.ae/jobs/Sr.-Developer---Fraud-Management">Sales Specialist - MS Office 365 </a> </h5> </b></td>
+     
+    </tr>
+	
+	<tr>
+     
+       <td><img src="images/task1a.png" alt="image not found" width="100px;" height="100px;"> </td>
+      <td><h5> <b> <a href="https://www.omb.ae/jobs/Sr.-Developer---Fraud-Management">Remote Account Manager </a> </h5> </b></td>
+     
+    </tr>
+	
+	<tr>
+     
+       <td><img src="images/task1a.png" alt="image not found" width="100px;" height="100px;"> </td>
+      <td><h5> <b> <a href="https://www.omb.ae/jobs/Sr.-Developer---Fraud-Management">Senior Manager-prepaid & OTT communication </a>  </h5></b></td>
+     
+    </tr>
+	
+	<tr>
+     
+       <td><img src="images/task1a.png" alt="image not found" width="100px;" height="100px;"> </td>
+      <td><h5><b> <a href="https://www.omb.ae/jobs/Sr.-Developer---Fraud-Management">Database Administrator - MongoDB </a>  </h5> </b></td>
+     
+    </tr>
+	
+	<tr>
+     
+       <td><img src="images/task1a.png" alt="image not found" width="100px;" height="100px;"> </td>
+      <td><h5> <b><a href="https://www.omb.ae/jobs/Sr.-Developer---Fraud-Management">Technician - Special Events and Mission Local Staff Enhancements (FO&P) </a></h5></b></td>
+     
+    </tr>
+	
+	<tr>
+     
+       <td><img src="images/task1a.png" alt="image not found" width="100px;" height="100px;"> </td>
+      <td><h5><b> <a href="https://www.omb.ae/jobs/Sr.-Developer---Fraud-Management">Technician - Field Operations and Projects (FO&P) </a>  </h5></b></td>
+     
+    </tr>
+	
+	<tr>
+       <td><img src="images/task1a.png" alt="image not found" width="100px;" height="100px;"> </td>
+      <td><h5> <b> <a href="https://www.omb.ae/jobs/Sr.-Developer---Fraud-Management">Technician - CSE Field Technicians (CS, BS, MOI) </a></h5></b></td>
+     
+    </tr>
+	
+	<tr>
+       <td><img src="images/task1a.png" alt="image not found" width="100px;" height="100px;"> </td>
+      <td><h5> <b><a href="https://www.omb.ae/jobs/Sr.-Developer---Fraud-Management">Technician - CSE Back Office</a>  </h5></b></td>
+     
+    </tr>
+	
+	<tr>
+       <td><img src="images/task1a.png" alt="image not found" width="100px;" height="100px;"> </td>
+      <td><h5> <b> <a href="https://www.omb.ae/jobs/Sr.-Developer---Fraud-Management">Technician - Data Centre Support</a> </h5></b></td>
+     
+    </tr>
+	<tr>
+       <td><img src="images/task1a.png" alt="image not found" width="100px;" height="100px;"> </td>
+      <td><h5> <b>   <a href="https://www.omb.ae/jobs/Sr.-Developer---Fraud-Management">Technician - Satellite operations and VVIP Missions</a> </h5> </b></td>
+    
+    </tr>
+  </tbody>
+</table>
+</div>
+ </div>
+<div class="min_wrapper"> 
+</div>
+</div>
+</body>
+</html>
